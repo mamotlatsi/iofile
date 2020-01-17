1bae29de9a59e325229c5888022e09e071f2c021
mamotlatsi <mamotlatsi.makhate@umuzi.org>
2020-01-17 15:07:17 +0200

    filelogio

---
+++ src/test/java/VisitorTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VisitorTest {
+
+    @Test
+    void save() {
+    }
+
+class loadTest{
+        @Test
+    void load(){
+
+        }
+}
+}
